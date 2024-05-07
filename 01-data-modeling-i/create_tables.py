4cdbbd210b196d9a31b3a2f682ded9e4c1b37f03
Alohalong <90435137+Alohalong@users.noreply.github.com>
2024-05-07 02:01:53 +0000

    - Create repositories table - add FK repo on events table - add url and display

--- 01-data-modeling-i/create_tables.py
+++ 01-data-modeling-i/create_tables.py
@@ -8,30 +8,50 @@ PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_repositories = "DROP TABLE IF EXISTS repositories"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
         id int,
         login text,
+        display text,
+        url text,
         PRIMARY KEY(id)
     )
 """
+
+table_create_repositories = """
+    CREATE TABLE IF NOT EXISTS repositories (
+        id int,
+        name text,
+        actor_id int,
+        PRIMARY KEY (id),
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+    )
+"""
+
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
         id text,
         type text,
         actor_id int,
+        repo_id int,
+        create_at text,
         PRIMARY KEY(id),
+        CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repositories(id),
         CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
     )
 """
 
 create_table_queries = [
     table_create_actors,
+    table_create_repositories,
     table_create_events,
+
 ]
 drop_table_queries = [
     table_drop_events,
+    table_drop_repositories,
     table_drop_actors,
 ]
 
